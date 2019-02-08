b17c391c6f5efcb73db343a318c67abf13018ff8
Dmytro Lesyshyn <dima.lesyshyn@gmail.com>
2019-02-08 11:25:48 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="main.css">
+	<title>Test Git</title>
+</head>
+<body>
+	<h1>This is test for Git Remote!</h1>
+	<img id="medium_img"src="git_logo.png" alt="This is git logo">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi quaerat, impedit enim totam, suscipit assumenda doloribus laudantium quae dicta. Aperiam nam quod enim, omnis. Odit quos vitae repudiandae, facere pariatur.</p>
+</body>
+</html>
